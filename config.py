9ff02b99d59f01efa654f635dde42e383eb3b5dc
Joel Itty Titus <joelitty001@gmail.com>
2025-12-25 12:24:01 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 32
+EPOCHS = 10
+
+DATASET_DIR = "dataset"
+MODEL_PATH = "saved_model/cnn_leaf_model.h5"
+
+CLASS_NAMES = [
+    "Healthy",
+    "Leaf_Blight",
+    "Leaf_Spot",
+    "Rust"
+]
